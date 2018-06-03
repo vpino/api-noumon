aff3795b65527a991f17112e5bb9bbeb6d099049
Victor Pino <victopin0@gmail.com>
2018-06-03 18:33:22 -0400

    feat(app): Install mongo and add connection to mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/api/product', (req, res) => {
-	res.status(200).send({ products: []})
+  res.status(200).send({ products: []})
 })
 
 app.get('/api/product/:productId', (req, res) => {
@@ -18,8 +19,8 @@ app.get('/api/product/:productId', (req, res) => {
 })
 
 app.post('/api/product', (req, res) => {
-	console.log(req.body)
-	res.status(200).send({ message: `Product received successfully`})
+  console.log(req.body)
+  res.status(200).send({ message: `Product received successfully`})
 })
 
 app.put('/api/product/:productId', (req, res) => {
@@ -30,6 +31,14 @@ app.delete('/api/product/:productId', (req, res) => {
 
 })
 
-app.listen(port, () => {
-	console.log(`API REST MONGOJS EXPRESS ${port}`)
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+  if (err) {
+    return console.log(`Error to connect to the database: ${err}`)
+  }
+  console.log('Conection established to the database...')
+    
+  app.listen(port, () => {
+    console.log(`API REST MONGOJS EXPRESS ${port}`)
+  })
 })
+
