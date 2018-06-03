abca196364475ff40ef23aa9368841d38bc2be8f
Victor Pino <victopin0@gmail.com>
2018-06-03 18:59:13 -0400

    feat(app): Add model Product and Add logic to save a product in the database

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Product = require('./models/product')
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -19,8 +21,21 @@ app.get('/api/product/:productId', (req, res) => {
 })
 
 app.post('/api/product', (req, res) => {
+  console.log('POST /api/product')
   console.log(req.body)
-  res.status(200).send({ message: `Product received successfully`})
+
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.picture
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description = req.body.description
+
+  product.save((err, productStored) => {
+    if (err) res.status(500).send({ message: `Error in save the object: ${err}` })
+    
+    res.status(200).send({product: productStored})
+  })
 })
 
 app.put('/api/product/:productId', (req, res) => {
