1830bcb70def1effa18cc2303388f569a762e8fb
Victor Pino <victopin0@gmail.com>
2018-06-03 18:02:48 -0400

    feat(app): Create the struct of the APP

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+	console.log(`API REST MONGOJS EXPRESS ${port}`)
+})
