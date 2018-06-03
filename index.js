8375d874903a263afa67065debb2011d5f3fdcf6
Victor Pino <victopin0@gmail.com>
2018-06-03 18:09:31 -0400

    feat(app): Create a endpoint and pass 1 parameter

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/hi/:name', (req, res) => {
+	res.send({ message: `Hi ${req.params.name}`})
+})
 
 app.listen(port, () => {
 	console.log(`API REST MONGOJS EXPRESS ${port}`)
